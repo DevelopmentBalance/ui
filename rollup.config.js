cddb47320578a790899442e50da668bfe50a8c4f
Calenzo <calenzodevs@gmail.com>
2023-04-25 14:26:38 -0300

    fix: I was changed the publish of github npm to npm

--- rollup.config.js
+++ rollup.config.js
@@ -4,29 +4,22 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
 
-const packageJson = require("./package.json");
-
 export default {
   input: "src/index.ts",
-  output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true
-    }
-  ],
+  output: {
+    dir: "lib",
+    format: "esm",
+    sourcemap: true,
+    preserveModules: true,
+    preserveModulesRoot: "src",
+  },
   plugins: [
     peerDepsExternal(),
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     postcss({
-        extensions: ['.css']
-    })
-  ]
+      extensions: [".css"],
+    }),
+  ],
 };
