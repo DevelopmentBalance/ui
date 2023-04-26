6b6246fc221398f4b04c1d87bd556539b41ffdc0
Ramon Xavier <ramon1234rxm@gmail.com>
2023-04-26 17:47:53 -0300

    chore: move to esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,19 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
-
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import image from '@rollup/plugin-image';
 
-
-const packageJson = require("./package.json");
-
 export default [
   {
     input: "src/index.ts",
-    output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
+    output: {
+      dir: "lib",
+      format: "esm",
+      sourcemap: true,
+    },
     plugins: [
       peerDepsExternal(),
       resolve(),
@@ -35,12 +23,5 @@ export default [
       terser(),
       image()
     ],
-  },
-  {
-    input: "lib/esm/types/index.d.ts",
-    output: [{ file: "lib/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-
-    external: [/\.css$/], // telling rollup anything that is .css aren't part of type exports 
   },
 ]
